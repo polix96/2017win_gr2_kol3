ce3c06b927c2533c961b691e78138318ebf8f12d
Paulina Olejniczak <5olejniczak@taurus.fis.agh.edu.pl>
2017-11-23 17:50:22 +0100

    add tests

--- kol3.py
+++ kol3.py
@@ -3,10 +3,19 @@ import kol1
 import unittest
 
 
-class TestCorrectTilt(unittest.TestCase):
-	def amount_of_cycles_input(self):
-		self.assertEqual(self.amount_of_cycles_input,5)
+class TestPlane(unittest.TestCase):
+	def setUp(self):
+		weather1 = kol1.Weather()
+		self.plane=kol1.Plane("Kajtek",30,weather1)
 
+	def test_name_of_plane(self):
+		self.assertEqual(self.plane.name,"Kajtek")
+	
+	def test_tilt_of_plane(self):
+		self.assertEqual(self.plane.tilt,30)
+
+	def test_amount_of_cycles_input(self):
+		self.assertEqual(self.plane.amount_of_cycles_input(),10)
 
 if __name__ == '__main__':
 	unittest.main()
